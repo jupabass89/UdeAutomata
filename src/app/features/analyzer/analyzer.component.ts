ae93b78c5a5bf97ee5eeb26ad1e17a9fb9f59caa
Pablo Gomez Restrepo <pablo.gomezr@perficient.com>
2022-02-13 12:37:18 -0500

    agregando el input para el automata fix

--- src/app/features/analyzer/analyzer.component.ts
+++ src/app/features/analyzer/analyzer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AnalyzerService } from '../services/analyzer.service';
+import { InputModalComponent } from './input-modal/input-modal.component';
 import { IAutomata } from './interfaces/IAutomata';
 @Component({
   selector: 'app-analyzer',
